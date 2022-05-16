01619478fa5a726ab9bf2f7c4990e6c9c81783d3
Mohamed Salah <immortal.mind2016@gmail.com>
2022-05-16 12:29:59 +0200

    feature: add our text parser function

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,1 +1,24 @@
-export const parseTextToOject = (text) => {};
+export const parseTextToOject = (text) => {
+  const lines = text.split("\n");
+
+  //extract anything between these two characters []
+  const timeRegex = /(?<=\[).*(?=])/g;
+  //extract anything after this character [
+  const textRegex = /(?<=]).*/g;
+
+  return lines.map((line) => {
+    if (line == " ") {
+      return null;
+    }
+    const index = line.split(" ")[0];
+    const time = line.match(timeRegex)?.[0];
+
+    const text = line.match(textRegex)?.[0].trim("");
+
+    return {
+      index: Number(index),
+      time,
+      text,
+    };
+  });
+};
