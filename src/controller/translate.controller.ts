0f054f7d378d7a2c46363fd60e382f272ff91e69
Mohamed Salah <immortal.mind2016@gmail.com>
2022-05-16 12:33:11 +0200

    feature: user our parser function

--- src/controller/translate.controller.ts
+++ src/controller/translate.controller.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { parseTextToOject } from "../utils/parser";
 
 export const translateFile = (req, res) => {
   if (!req.file) {
@@ -7,4 +8,5 @@ export const translateFile = (req, res) => {
       .json({ error: "Your input doesn't contain a required file" });
   }
   const fileData = req?.file?.buffer.toString("utf-8");
+  const parsedFileData = parseTextToOject(fileData);
 };
