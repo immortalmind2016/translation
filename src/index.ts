bfe8a2752de8db4de186e3a646707d761146002a
Mohamed Salah <immortal.mind2016@gmail.com>
2022-05-17 14:15:38 +0200

    feature: init swagger and add translate docs

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import express from "express";
 import config from "./config";
 import translateRouter from "./route/translate.routes";
 import mongoose from "mongoose";
+import swaggerUi from "swagger-ui-express";
+import swaggerJs from "swagger-jsdoc";
+import { swaggerOptions } from "./config/swaggerConfig";
 mongoose.connect(config.MONGO_URI);
+
 const app = express();
+const swaggerDocument = swaggerJs(swaggerOptions);
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/translate", translateRouter);
 app.listen(config.PORT, () => {
   console.log(`The service has been started on port ${config.PORT}`);
--- src/route/translate.routes.ts
+++ src/route/translate.routes.ts
@@ -3,8 +3,34 @@ import { importData, translateFile } from "../controller/translate.controller";
 import upload from "../config/multerConfig";
 
 const translateRouter = Router();
-
+/**
+ * @openapi
+ * "/translate":
+ *   post:
+ *     summary: Translate your subtitle file
+ *     requestBody:
+ *       description: Test
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *              schema:
+ *                  properties:
+ *                      file:
+ *                          type: string
+ *                          format: binary
+ *
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ *         content:
+ *             application/json:
+ *                  schema:
+ *                     properties:
+ *                          message:
+ *                              type: string
+ *                              example: Subtitles have been successfully translated, We will send it ASAP to your email
+ */
 translateRouter.post("/", upload.single("file"), translateFile);
+
 translateRouter.post("/import-data", importData);
 
 export default translateRouter;
