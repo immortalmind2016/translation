bde83be23b8f02d7f359486fdb333ebbfc5b3898
Mohamed Salah <immortal.mind2016@gmail.com>
2022-06-02 00:04:58 +0200

    fix:issues

--- src/seed/text.ts
+++ src/seed/text.ts
@@ -1,9 +1,5 @@
-import { TextModel } from "../model/translate.model";
-import {
-  Language,
-  TextTranslationDocument,
-  TextTranslationStatus,
-} from "../types";
+import { TextModel } from "../modules/translation/translation.model";
+import { TextTranslationDocument, TextTranslationStatus } from "../types";
 import mongoose from "mongoose";
 import config from "../config";
 mongoose.connect(config.MONGO_URI);
@@ -12,9 +8,9 @@ const text_1: Omit<TextTranslationDocument, "_id" | "createdAt" | "updatedAt"> =
   {
     text: "I am Arwen - I've come to help you.",
     translations: {
-      [Language.de]: "Ich bin Arwen - Ich bin gekommen, um dir zu helfen.",
+      de: "Ich bin Arwen - Ich bin gekommen, um dir zu helfen.",
     },
-    defaultLanguage: Language.en,
+    defaultLanguage: "en",
     status: TextTranslationStatus.APPROVED,
   };
 
@@ -22,9 +18,9 @@ const text_2: Omit<TextTranslationDocument, "_id" | "createdAt" | "updatedAt"> =
   {
     text: "Come back to the light.",
     translations: {
-      [Language.de]: "Komm zurück zum Licht.",
+      de: "Komm zurück zum Licht.",
     },
-    defaultLanguage: Language.en,
+    defaultLanguage: "en",
     status: TextTranslationStatus.APPROVED,
   };
 const seed = async () => {
